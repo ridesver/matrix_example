a2db9c10d73e46494f19fb006302d178cd3ac5f5
justcppdev <justcppdev@ya.ru>
2018-03-03 08:18:33 +0300

    Update matrix.cpp

--- tests/matrix.cpp
+++ tests/matrix.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <catch.hpp>
 #include <sstream>
 
@@ -28,4 +29,45 @@ TEST_CASE("reading matrix")
     matrix.write( ostream );
     
     REQUIRE( input == ostream.str() );
+}
+
+matrix_t matrix( std::string const & representation )
+{
+    matrix_t result;
+    
+    std::istringstream istream{ representation };
+    assert( result.read( istream ) );
+    
+    return result;
+}
+
+std::string representation( matrix_t const & matrix )
+{
+    std::ostringstream ostream;
+    matrix.write( ostream );
+    
+    return ostream.str();
+}
+
+TEST_CASE("addings matrixs")
+{
+    std::string first_matrix_representation{
+        "1, 3\n"
+        "1 1 1\n"
+    };
+    std::string second_matrix_representation{
+        "1, 3\n"
+        "1 1 1\n"
+    };
+    matrix_t first_matrix = matrix( first_matrix_representation );
+    matrix_t second_matrix = matrix( second_matrix_representation );
+    
+    matrix_t result_matrix = first_matrix + second_matrix;
+
+    std::string expected_result_matrix_representation{
+        "1, 3\n"
+        "2 2 2\n"
+    };
+    
+    REQUIRE( representation( result_matrix ) == expected_result_matrix_representation );
 }
