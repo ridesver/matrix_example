76ff23ac295354d039179bc7ef5971012988a344
Daria Eremitskaya <31545001+ridesver@users.noreply.github.com>
2018-03-18 20:08:14 +0300

    Update example.cpp

--- examples/example.cpp
+++ examples/example.cpp
@@ -6,11 +6,11 @@ int main()
 {
 	matrix_t a, b;
 	char op;
-	if( a.read( std::cin ) && std::cin >> op && b.read( std::cin ) ) {
-		if( op == '+' ) {
-			( a + b ).write( std::cout );
+	if (a.read(std::cin) && std::cin >> op && b.read(std::cin)) {
+		if (op == '+') {
+			(a + b).write(std::cout);
 		}
 	}
-    
-    return 0;
+
+	return 0;
 }
